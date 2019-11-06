83604e1bd0a7ab5ca7e6449a5b2d7ae6c2093b21
FibZIII <soloninandr@gmail.com>
2019-11-06 21:44:15 +0200

    book

--- FirstShop/apps/products/models.py
+++ FirstShop/apps/products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from apps.catalog.models import Author, Gengre
+#from apps.catalog.models import Author, Gengre, Publishing_house
 
 class Product(models.Model):
 	product_name = models.CharField(max_length = 200)
@@ -11,6 +11,9 @@ class Product(models.Model):
 '''class Books(Product):
 	author_name= models.ForeignKey(Author, on_delete=models.SET_NULL, null = True)
 	gengre = models.ForeignKey(Gengre, on_delete=models.SET_NULL, null = True)
+	publishing_house = models.ForeignKey(Publishing_house, on_delete=models.SET_NULL, null = True)
+	Publication_date = models.DateTimeField() 
+	Number_of_pages = models.IntegerField( default = 0)
 	def __str__(self):
 		return self.product_name'''
 
