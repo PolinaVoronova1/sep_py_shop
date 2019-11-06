4a3428d6faa904e5303ae480b81e7cd36d1f283a
FibZIII <soloninandr@gmail.com>
2019-11-06 20:55:29 +0200

    product

--- FirstShop/apps/products/models.py
+++ FirstShop/apps/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+#from apps.catalog.models import Author, Gengre
 
-# Create your models here.
+class Product(models.Model):
+	product_name = models.CharField(max_length = 200)
+	product_price = models.	(default = 0)
+	product_avaliable_count = models.IntegerField( default = 0)
+	product_detail = models.TextField(default = "", editable = True)
+	def __str__(self):
+		return self.product_name
+'''class Books(Product):
+	author_name= models.ForeignKey(Author, on_delete=models.SET_NULL, null = True)
+	gengre = models.ForeignKey(Gengre, on_delete=models.SET_NULL, null = True)
+	def __str__(self):
+		return self.product_name'''
+
+
