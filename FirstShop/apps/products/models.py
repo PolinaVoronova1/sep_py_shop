3157872745ca47906e33f2776adb4ea3b6af3285
FibZIII <soloninandr@gmail.com>
2019-11-09 21:24:12 +0200

    product_v0.2

--- FirstShop/apps/products/models.py
+++ FirstShop/apps/products/models.py
@@ -2,23 +2,21 @@ from django.db import models
 #from apps.catalog.models import Author, Gengre, Publishing_house
 
 class Product(models.Model):
-	#FIXME: no book specific fields
-	#FIXME: no created-updated fields
-	#FIXME: can be sold field
-	product_name = models.CharField(max_length = 200)
-	product_price = models.	(default = 0)#FIXME: invalid syntax
-	product_avaliable_count = models.IntegerField( default = 0)
-	product_detail = models.TextField(default = "", editable = True)
-	def __str__(self):
-		return self.product_name#FIXME: invalid ident 
-#FIXME: remove comment
-'''class Books(Product):
-	author_name= models.ForeignKey(Author, on_delete=models.SET_NULL, null = True)
-	gengre = models.ForeignKey(Gengre, on_delete=models.SET_NULL, null = True)
-	publishing_house = models.ForeignKey(Publishing_house, on_delete=models.SET_NULL, null = True)
-	Publication_date = models.DateTimeField() 
-	Number_of_pages = models.IntegerField( default = 0)
-	def __str__(self):
-		return self.product_name'''
 
+
+	product_name = models.CharField(max_length = 200)
+	product_price = models.FloatField(default = 0)
+	product_avaliable_count = models.PositiveIntegerField(default = 0, null = True)
+	product_detail = models.TextField(default = "", editable = True)
+	product_can_be_sold = models.BooleanField(default = True)
+	product_created_updated = models.DateTimeField(auto_now_add = True)
+	def __str__(self):
+		return self.product_name
+class Book(Product):
+	#author_name= models.ForeignKey(Author, on_delete=models.SET_NULL, null = True)
+	#gengre = models.ManyToManyField(Gengre)
+	#publishing_house = models.ForeignKey(Publishing_house, on_delete=models.SET_NULL, null = True)
+	Publication_date = models.DateTimeField() 
+	Number_of_pages = models.IntegerField(default = 0)
+	
 
